063683340eb8281238f025935d834953320ef3c3
Felipe Santos <felipecassiors@gmail.com>
2020-03-05 04:38:52 +0000

    feat: add swagger support

--- src/main/java/com/example/springcrud/SpringCrudApplication.java
+++ src/main/java/com/example/springcrud/SpringCrudApplication.java
@@ -2,12 +2,19 @@ package com.example.springcrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
+@EnableSwagger2WebMvc
+@Import({ SpringDataRestConfiguration.class, BeanValidatorPluginsConfiguration.class })
 public class SpringCrudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCrudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCrudApplication.class, args);
+    }
 
 }
